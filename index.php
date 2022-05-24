e8c65ba69612af49288874a032b8493acd6cb20e
sofiane <sofiane@meriane.dev>
2022-05-24 10:12:46 +0200

    Projekt aktualisiert

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
+<?php
+    $header = "<header>Header</header>";
+    $nav = "<nav>Navigation</nav>";
+    $content = "<section>Content</section>";
+    $footer = "<footer>Footer</footer>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" type="text/css">
     <title>Erste Document</title>
 </head>
-<body>  
-    <h1>Hello PHP 2022!</h1>
+<body>
+
+    <?php
+        echo $header.$nav.$content.$footer;
+    ?>
 </body>
-</html>     
+</html>
